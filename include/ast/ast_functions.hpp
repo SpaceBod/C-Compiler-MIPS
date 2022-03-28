cd3b23931d758f5443c3fccaf6142014123a3099
xc720 <xc720@ic.ac.uk>
2022-03-28 05:37:24 +0100

    Stack implemented in Function

--- include/ast/ast_functions.hpp
+++ include/ast/ast_functions.hpp
@@ -16,6 +16,12 @@ public:
     Function(Variable *_name, StatPtr _stat = nullptr)
         : name(_name), stat(_stat)
     {}
+     Function(Variable *_name, ExpressionPtr _arg = nullptr, StatPtr _stat = nullptr)
+        : name(_name)
+        , arg(_arg) 
+        , stat(_stat)
+    {}
+
 
     virtual ~Function()
     {
@@ -24,12 +30,12 @@ public:
         delete stat;
     }
 
-    const std::string getFunction() const
+    const std::string returnFunc() const
     {
-        return name->return_var_name();
+        return name->getVar_name();
     }
 
-    ExpressionPtr getArg() const
+    ExpressionPtr returnArg() const
     {
         return arg;
     }
@@ -45,8 +51,36 @@ public:
 
     void Translate2MIPS(std::string destReg) const
     {
-        std::cout << name->getId() << ":" << std::endl;
+        std::cout << name->getVar_name() << ":" << std::endl;
+        std::cout << "addiu $sp, $sp, -44" << std::endl;
+        std::cout << "sw $s0, 4($sp)" << std::endl;
+        std::cout << "sw $s1, 8($sp)" << std::endl;
+        std::cout << "sw $s2, 12($sp)" << std::endl;
+        std::cout << "sw $s3, 16($sp)" << std::endl;
+        std::cout << "sw $s4, 20($sp)" << std::endl;
+        std::cout << "sw $s5, 24($sp)" << std::endl;
+        std::cout << "sw $s6, 28($sp)" << std::endl;
+        std::cout << "sw $s7, 32($sp)" << std::endl;
+        std::cout << "sw $fp, 36($sp)" << std::endl;
+        std::cout << "sw $ra, 40($sp)" << std::endl;
+        StackPtr.setIncrement(40);
+
         stat->Translate2MIPS(destReg);
+
+        StackPointer.setIncrement(0);
+        std::cout << "lw $s0, 4($sp)" << std::endl;
+        std::cout << "lw $s1, 8($sp)" << std::endl;
+        std::cout << "lw $s2, 12($sp)" << std::endl;
+        std::cout << "lw $s3, 16($sp)" << std::endl;
+        std::cout << "lw $s4, 20($sp)" << std::endl;
+        std::cout << "lw $s5, 24($sp)" << std::endl;
+        std::cout << "lw $s6, 28($sp)" << std::endl;
+        std::cout << "lw $s7, 32($sp)" << std::endl;
+        std::cout << "lw $fp, 36($sp)" << std::endl;
+        std::cout << "lw $ra, 40($sp)" << std::endl;
+        std::cout << "move $fp, $sp" << std::endl;
+        std::cout << "jr $ra" << std::endl;
+
     }
 };
 
