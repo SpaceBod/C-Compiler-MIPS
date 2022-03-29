666667136492c3e3a0a623b4753a72468d173e35
Gian-Luca Fenocchi <lucabod8@gmail.com>
2022-03-29 22:02:42 +0100

    updated files
    Co-Authored-By: xc720 <79037211+xc720@users.noreply.github.com>

--- mips.s
+++ mips.s
@@ -1,58 +1,36 @@
-f:
-PUSH0:
+.text
+main:
+move $fp, $sp
 addiu $sp, $sp, -44
-sw $ra, 40($sp)
-nop
-sw $fp, 36($sp)
-nop
 sw $s0, 4($sp)
-nop
 sw $s1, 8($sp)
-nop
 sw $s2, 12($sp)
-nop
 sw $s3, 16($sp)
-nop
 sw $s4, 20($sp)
-nop
 sw $s5, 24($sp)
-nop
 sw $s6, 28($sp)
-nop
 sw $s7, 32($sp)
-nop
-move $fp, $sp
-body2:
-li $v0, 42
-addiu $sp, $sp, 0
-move $fp, $sp
-b POP1
-nop
-addiu $sp, $sp, 0
-move $fp, $sp
-POP1:
-mtc1 $v0, $f0
+sw $fp, 36($sp)
+sw $ra, 40($sp)
+addi $sp, $sp, -4
+addi $t0, $0, 2
+sw $t0, -48($fp)
+addi $t0, $0, 999
+add $v0, $0, $t0
+j mainend
+addiu $sp, $sp, 4
+mainend:
+addiu $sp, $sp, 4
 lw $s0, 4($sp)
-nop
 lw $s1, 8($sp)
-nop
 lw $s2, 12($sp)
-nop
 lw $s3, 16($sp)
-nop
 lw $s4, 20($sp)
-nop
 lw $s5, 24($sp)
-nop
 lw $s6, 28($sp)
-nop
 lw $s7, 32($sp)
-nop
-lw $ra, 40($sp)
-nop
 lw $fp, 36($sp)
-nop
+lw $ra, 40($sp)
 move $sp, $fp
 jr $ra
-nop
-.global f
+.global main
